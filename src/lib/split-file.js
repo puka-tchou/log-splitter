9d0384cb729d1cda1545d52c4a1aa959666ea0da
Gaspard Bariller <gaspard.bariller@tutanota.com>
2019-08-26 09:53:57 +0200

    feature(split-file.js): Handle potential errors

--- src/lib/split-file.js
+++ src/lib/split-file.js
@@ -6,16 +6,18 @@ const fs = require("fs");
  * @param {String} rawPath The path of the file to parse
  */
 const parseFile = rawPath => {
+  let fileByLine = [];
+  try {
   // Normalize path
   const truePath = path.normalize(rawPath);
   // Read raw file content as a string
   const fileContent = fs.readFileSync(truePath, "utf8");
   // Split raw file into an array at each line break
-  const fileByLine = fileContent.split(/\r\n|\n/);
-  console.log(fileByLine.length);
-  fileByLine.forEach(line => {
-    // Do stuff line by line
-  });
+    fileByLine = fileContent.split(/\r\n|\n/);
+  } catch (error) {
+    return error;
+  }
+  return fileByLine;
 };
 
 module.exports = { parseFile: parseFile };
