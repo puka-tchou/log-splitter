1fcbcec677b71ad8445d7d5e0c87121e6436b0ee
Gaspard Bariller <gaspard.bariller@tutanota.com>
2019-08-26 16:19:55 +0200

    feature: Handle errors in writeFiles()

--- src/lib/write-files.js
+++ src/lib/write-files.js
@@ -9,20 +9,24 @@ const path = require("path");
  */
 const writeFiles = (name, folder, files) => {
   let index = 0;
-
-  files.forEach(file => {
-    let fileName = path.join(folder, `${name}_${index.toString()}.log`);
-    index++;
-    console.debug(`Writing ${fileName}`);
-    // Open a stream
-    const stream = fs.createWriteStream(fileName, { flags: "a" });
-    // Append to the file
-    file.forEach(line => {
-      stream.write(`${line}\n`);
+  try {
+    files.forEach(file => {
+      let fileName = path.join(folder, `${name}_${index.toString()}.log`);
+      index++;
+      console.debug(`Writing ${fileName}`);
+      // Open a stream
+      const stream = fs.createWriteStream(fileName, { flags: "a" });
+      // Append to the file
+      file.forEach(line => {
+        stream.write(`${line}\n`);
+      });
+      // Close the stream
+      stream.end();
     });
-    // Close the stream
-    stream.end();
-  });
+  } catch (error) {
+    return error;
+  }
+  return true;
 };
 
 module.exports = { writeFiles: writeFiles };
