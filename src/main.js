6c5bd48f76203dc4c3b086b949fce8a88a353a8f
Gaspard Bariller <gaspard.bariller@tutanota.com>
2019-08-26 17:43:09 +0200

    improve: Error handling

--- src/main.js
+++ src/main.js
@@ -14,43 +14,54 @@ const { displayProgress } = require("./lib/display-progress");
 const main = mainWindow => {
   let selectedFile = [],
     files = [];
+
   // Listen to ipc.send in index.html
   ipcMain.on("choose-file", (event, data) => {
     // Start dialog handler
     selectedFile = modalDialog(mainWindow, "open");
-    // Display the file being processed
-    displayProgress(mainWindow, selectedFile[0]);
-    // Parse the file
-    const rawFile = parseFile(selectedFile[0]);
-    // Display the number of lines
-    displayProgress(
-      mainWindow,
-      `Recherche de correspondance dans ${files.length} lignes...`
-    );
-    // Split the file
-    files = splitFile(rawFile);
-    // Display the number of matches
-    displayProgress(mainWindow, `${files.length} correspondances trouvées`);
-    // Send the result to ipc.renderer in index.html
+    if (typeof selectedFile !== "undefined") {
+      // Display the file being processed
+      displayProgress(mainWindow, selectedFile[0]);
+      // Parse the file
+      const rawFile = parseFile(selectedFile[0]);
+      // Display the number of lines
+      displayProgress(
+        mainWindow,
+        `Recherche de correspondance dans ${files.length} lignes...`
+      );
+      // Split the file
+      files = splitFile(rawFile);
+      // Display the number of matches
+      displayProgress(mainWindow, `${files.length} correspondances trouvées`);
+      // Send the result to ipc.renderer in index.html
+    }
     event.returnValue = true;
   });
+
   ipcMain.on("save-file", (event, data) => {
     // Ask where to save the files
     const folder = modalDialog(mainWindow, "save");
-    // Display the progress
-    displayProgress(
-      mainWindow,
-      `Enregistrement de ${files.length} fichiers vers ${folder[0]}`
-    );
-    // Write the files to the disk
-    writeFiles(path.parse(selectedFile[0]).name, folder[0], files, mainWindow);
+    if (typeof folder !== "undefined") {
+      // Display the progress
+      displayProgress(
+        mainWindow,
+        `Enregistrement de ${files.length} fichiers vers ${folder[0]}`
+      );
+      // Write the files to the disk
+      writeFiles(
+        path.parse(selectedFile[0]).name,
+        folder[0],
+        files,
+        mainWindow
+      );
+      displayProgress(
+        mainWindow,
+        `${
+          files.length
+        } fichiers ont été enregistrés avec succès dans le dossier ${folder[0]}`
+      );
+    }
     event.returnValue = true;
-    displayProgress(
-      mainWindow,
-      `${
-        files.length
-      } fichiers ont été enregistrés avec succès dans le dossier ${folder[0]}`
-    );
   });
 };
 
